cc8a159b6711fe9428157b8d15d4da0c584320c4
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-09-29 03:58:47 +0000

    Added yellow button and green button functionality

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ butt1, butt2, pinG, pinY, pinB=20,21,26,19,13
 
 
 GPIO.setup(pinG, GPIO.OUT)          #The Green LED
-my_pwm_green= GPIO.PWM(pinG, 100)     # create PWM object at 100Hz
+my_pwm_green= GPIO.PWM(pinG, 1000)     # create PWM object at 1000Hz for smoothness
 GPIO.setup(pinY, GPIO.OUT)          #The Yellow LED
-my_pwm_yellow= GPIO.PWM(pinY, 100)    # create PWM object at 100Hz
+my_pwm_yellow= GPIO.PWM(pinY, 1000)    # create PWM object at 1000Hz for smoothness
 GPIO.setup(pinB, GPIO.OUT)          #the Blue LED that blinks on and off
 GPIO.setup(butt1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #The first Button
 GPIO.setup(butt2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #The second button
@@ -18,20 +18,29 @@ GPIO.setup(butt2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #The second button
 def inputChange(pin):
   
   print("input pin ", pin ," status changed to: ", GPIO.input(pin) )
-
-  my_pwm_yellow.start(0) #initiate the pwm object. starting it at 0% duty cycle
-  my_pwm_green.start(50) #initiate pwm object at 50% duty cycle
-  for dc in range(0,101,1): # loop duty cycle from 0 to 100. dc is the iterating variable
-    my_pwm_yellow.ChangeDutyCycle(dc) # set duty cycle
-    sleep (0.005)                       # sleep 5 ms. 5ms*100 is half second
-    print(dc)
-  for dc in range(101,0,-1): #range(start,stop,step)
-    print(dc)
-    my_pwm_yellow.ChangeDutyCycle(dc-1)
-    sleep(0.005) #half second ramp up, half second ramp down
-  print("all done, leaving function")
-  my_pwm_yellow.stop()
-  my_pwm_green.stop()
+  
+  if pin==pinY:
+    my_pwm_yellow.start(0) #initiate the pwm object. starting it at 0% duty cycle
+    for dc in range(0,101,1): # loop duty cycle, dc, from 0 to 100. 
+      my_pwm_yellow.ChangeDutyCycle(dc) # set duty cycle
+      sleep (0.005)                  # sleep 5 ms. 5ms*100 is half second. 
+      print(dc)
+    for dc in range(101,0,-1): #range(start,stop,step)
+      print(dc)
+      my_pwm_yellow.ChangeDutyCycle(dc-1)
+      sleep(0.005) #half second ramp up, half second ramp down. 
+    my_pwm_yellow.stop()
+  elif pin==pinG:
+    my_pwm_green.start(0) #initiate pwm object at 50% duty cycle
+    for dc in range(0,101,1): # loop duty cycle, dc, from 0 to 100. 
+      my_pwm_yellow.ChangeDutyCycle(dc) # set duty cycle
+      sleep (0.005)                  # sleep 5 ms. 5ms*100 is half second. 
+      print(dc)
+    for dc in range(101,0,-1): #range(start,stop,step)
+      print(dc)
+      my_pwm_yellow.ChangeDutyCycle(dc-1)
+      sleep(0.005) #half second ramp up, half second ramp down. 
+    my_pwm_green.stop()
 
 
 
