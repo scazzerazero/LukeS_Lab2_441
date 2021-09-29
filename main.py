52008696e33bb68e25dcd12b385195747055d1ef
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-09-29 00:54:58 +0000

    Created the Lab 2 code here

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-import RPi.GPIO as GPIO
-from time import sleep
+import RPi.GPIO as GPIO #import pythons rasberry pi library
+from time import sleep #importing library so we can use sleep command
+#need help? Text Nick or Rohith
+GPIO.setmode(GPIO.BCM) #Set pin numbering mode to BCM rather than BOARD
+#defining pin numbers
+butt1, butt2, pinG, pinY, pinB=20,21,26,19,13
 
-GPIO.setmode(GPIO.BCM)
-p = 4
-GPIO.setup(p, GPIO.OUT)
 
-pwm= GPIO.PWM(p, 100)          # create PWM object @ 100 Hz
+GPIO.setup(pinG, GPIO.OUT)          #The Green LED
+my_pwm_green= GPIO.PWM(pinG, 1)     # create PWM object at 1Hz
+GPIO.setup(pinY, GPIO.OUT)          #The Yellow LED
+my_pwm_yellow= GPIO.PWM(pinY, 1)    # create PWM object
+GPIO.setup(pinB, GPIO.OUT)          #the Blue LED that blinks on and off
+GPIO.setup(butt1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #The first Button
+GPIO.setup(butt2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #The second button
 
+#defining the threaded callback function
+def inputChange(pin):
+  print("input pin ", pin ," status changed to: ", GPIO.input(pin) )
+  my_pwm_yellow.start(0) #initiate the pwm object. starting it at 0% duty cycle
+  for dc in range(0,101,1):       # loop duty cycle from 0 to 100. dc is the iterating variable
+    my_pwm_yellow.ChangeDutyCycle(dc) # set duty cycle
+    sleep(0.01)                       # sleep 10 ms
+    if dc == 100:
+      for dc in range(101,0,-1): #range(start,stop,step)
+        my_pwm_yellow.ChangeDutyCycle(dc-1)
+        sleep(0.01)
+
+#checking for interrupt on the two button pins
+GPIO.add_event_detect(butt1, GPIO.RISING, callback=inputChange, bouncetime=300) #debouncing =300
+GPIO.add_event_detect(butt2, GPIO.RISING, callback=inputChange, bouncetime=300)
+
+#the main loop that runs always. Blinking blue LED
 try:
-  pwm.start(0)                  # initiate PWM at 0% duty cycle
-  while 1:
-    for dc in range(101):       #loop duty cycle from 0 to 100
-      pwm.ChangeDutyCycle(dc)   # set duty cycle
-      sleep(0.01)               #sleep 10 ms
-except KeyboardInterrupt:       # stop gracefully on ctrl-C
-    print('\nExiting')
+  while(True):
+    GPIO.output(pinB,0)
+    sleep(0.5)
+    GPIO.output(pinB,1)
+    sleep(0.5)
+except KeyboardInterrupt: # if user hits ctrl-C
+  print('\nExiting')
+except Exception as e:    # catch all other errors
+  print("\ne")
 
-pwm.stop()
+my_pwm_green.stop()
+my_pwm_yellow.stop()
 GPIO.cleanup()
