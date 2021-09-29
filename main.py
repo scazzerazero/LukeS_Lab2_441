21d2b148f8ef4f503458f296a27f824b3cb5b0dd
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-09-29 03:24:53 +0000

    Delete sleep portion

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def inputChange(pin):
   my_pwm_yellow.start(0) #initiate the pwm object. starting it at 0% duty cycle
   for dc in range(0,101,1):       # loop duty cycle from 0 to 100. dc is the iterating variable
     my_pwm_yellow.ChangeDutyCycle(dc) # set duty cycle
-    sleep (0.1)                       # sleep 10 ms
+    #sleep (0.1)                       # sleep 10 ms
     print(dc)
     #if dc == 100:
      # for dc in range(101,0,-1): #range(start,stop,step)
